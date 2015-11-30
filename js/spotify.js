63ef31d05c56101a18da23066578915d34cb65f3
Starr Z. Chen <chenstarr@gmail.com>
2015-11-29 21:23:24 -0500

    Artist search working

--- js/spotify.js
+++ js/spotify.js
@@ -1,10 +1,37 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/technologies/web-api/search/
 
+$(document).ready( function() {
+  $("#search").submit( function(evt){
+    evt.preventDefault();
+    $("#results").empty(); // Clears previous search results
+    var keyword = $("#search-keyword").val();
+    var type = $("#search-type").val();
+    type == "artist" ? searchByArtist(keyword) : searchByTrack(keyword); // Not sure if using ternary(sp?) operator correctly here, let's find out
+  });
+});
 
 function searchByArtist(keyword) {
+
   var url = 'http://ws.spotify.com/search/1/artist.json?q='+keyword;
+  $.ajax({
+    url: url,
+    type: 'GET',
+    dataType: 'json'
+  }).done( function(response) {
+
+    for (var i=0; i < response.artists.length; i++) {
+      $("#results").append("<li><a href='" + response.artists[i].href + "'>" + response.artists[i].name + "</a></li>");
+    }
+
+  }).fail( function() {
+    console.log("Oops, I don't think so...");
+  }).always( function() {
+    console.log("Beep boop");
+  });
+
 }
+
 
 
 function searchByTrack(keyword) {
